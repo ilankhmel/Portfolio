c03a8c31cfe3d359703fac1d95c1ba4001a4f93d
Ilan Khmelnitsky <ilankhmel@gmail.com>
2023-06-14 16:12:56 +0300

    fix: Content fix

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -6,9 +6,8 @@ export default function GetInTouch() {
       <span className="blue-code">04.What's Next?</span>
       <h2>Get In Touch</h2>
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I’ll
-        try my best to get back to you!
+        I'm open for work! Whether you have a question or just want to say hi,
+        I’ll try my best to get back to you!
       </p>
       <div className="btn-bg">
         <a
